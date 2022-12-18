18be51a79fdf420adb6b884ba7fc28ee9a65ad9c
ryuwc <wom05169@gmail.com>
2022-12-18 19:33:43 +0900

    props 기초

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 export default class ExpenseItem extends Component {
+  // props 받기
+  constructor(props) {
+    super(props);
+    this.title = props.title;
+    this.amount = props.amount;
+    this.date = props.date;
+  }
+
+
   render() {
+
     return (
-      <div className="expense-ite">
-        <div>March 28th 2021</div>
-        <div className="expense-item__description">
-          <h2>Car Insurance</h2>
-          <div className="expense-item__price">$294.67</div>
+      <div className='expense-item'>
+        <ExpenseDate date={this.date} />
+        <div className='expense-item__description'>
+          <h2>{this.title}</h2>
+          <div className='expense-item__price'>${this.amount}</div>
         </div>
       </div>
     );
