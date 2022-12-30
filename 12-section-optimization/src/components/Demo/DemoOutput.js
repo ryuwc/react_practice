d2f1a09f82381a63bd37085b31c373feb9df34cb
ryuwc <wom05169@gmail.com>
2022-12-30 23:28:56 +0900

    lottie-web 사용해보기

--- 12-section-optimization/src/components/Demo/DemoOutput.js
+++ 12-section-optimization/src/components/Demo/DemoOutput.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import MyParagraph from "./MyParagraph";
+import lottie from 'lottie-web';
 
 import $ from 'jquery';
 
@@ -8,14 +9,25 @@ $('p').on('click', () => {
 })
 
 function DemoOutput(props) {
-  console.log('DemoOutput RUNNING');
+  const likecontainer = useRef();
+  useEffect(()=>{
+    lottie.loadAnimation({
+      container: likecontainer.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay:false,
+      animationData:require("../../lotti/17100-food.json")
+    })
+
+  },[])
+
+  console.log(likecontainer);
   const newTest = props.show.sort((a, b) => a - b);
-
-
 
   return (
 
     <MyParagraph>
+      <div ref={likecontainer}></div>
       {props.show ? 'This is new!' : ''}
       <p>{newTest}</p>
     </MyParagraph>
